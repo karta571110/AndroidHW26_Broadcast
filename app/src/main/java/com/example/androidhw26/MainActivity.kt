91acf0560bddd7194489cdc515729678ccf11644
karta571110 <44608844+karta571110@users.noreply.github.com>
2019-12-13 09:05:03 +0800

    finish

---
+++ app/src/main/java/com/example/androidhw26/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.androidhw26
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val MY_MESSAGE = "com.givemepass.sendmessage"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        registerReceiver(mBroadcast, IntentFilter(MY_MESSAGE))
+
+        send_broadcast.setOnClickListener {
+            val intent = Intent()
+            intent.action = MY_MESSAGE
+            sendBroadcast(intent)
+        }
+    }
+
+    private val mBroadcast = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (MY_MESSAGE == intent?.action) {
+                AlertDialog.Builder(this@MainActivity)
+                        .setMessage("我收到了!")
+                        .setPositiveButton("確定") { _, _ -> }
+                        .show()
+            }
+        }
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(mBroadcast)
+    }
+}
